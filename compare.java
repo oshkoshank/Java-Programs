f8290448dd806f37a1838b1c00f103648b8bc155
oshkoshank <bandstars45@gmail.com>
2022-09-23 10:03:54 +0530

    commit_compare_23/09/2022

---
+++ compare.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class compare 
+{
+    public static void main(String[] args)
+     {
+        Scanner o = new Scanner(System.in);
+        System.out.println("Enter the First number: ");
+        int a = o.nextInt();
+        System.out.println("Enter the Second number: ");
+        int b = o.nextInt();
+        System.out.println("Enter the Third number: ");
+        int c = o.nextInt();
+        int larg = (a>b)?(a>c?a:c):((c>b)?c:b);
+        System.out.println("The largest number is "+ larg);
+        o.close();
+    }
+}
