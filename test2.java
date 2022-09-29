d8e9b4f768e96c1e373d4ee6d85b7150490a9d33
oshkoshank <bandstars45@gmail.com>
2022-09-29 12:36:45 +0530

    commit

---
+++ test2.java
@@ -0,0 +1,9 @@
+/**
+ * test2
+ */
+public class test2 {
+
+    public static void main(String[] args) {
+        qwSystem.out.println("ehllfjo;aidsnvoiusdbvosiench 0r9ejv poiaweyrf7324p 9; hft298t7f-");
+    }
+}
